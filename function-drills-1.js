091533d567d71dd5f5fcf8c712bc67bae3e8ce62
JTLiner925 <[jtliner925@gmail.com]>
2020-01-07 09:34:34 -0600

    added if statements and try catch

--- function-drills-1.js
+++ function-drills-1.js
@@ -1,6 +1,23 @@
+function getYearOfBirth(age){
+  return 2019 - age;
+}
+
 function createGreeting(name, age){
-  const yearOfBirth =  getYearOfBirth(age);
-  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}`;
+  if (name === undefined || age === undefined){
+    throw new Error('Arguments not valid.');
+  }
+
+  if (age < 0){
+    throw new Error('Age can not be negative.');
+  }
+
+  if (typeof age !== 'number') {
+    throw new TypeError('Age must be a number.');
+  }
+
+
+  const yob =  getYearOfBirth(age);
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yob}`;
 }
 //took const yearOfBirth out of createGreeting and made it it's own function
 //when I put the new value in the template literal that was returned in 
@@ -8,11 +25,11 @@ function createGreeting(name, age){
 
 //actually*** I realized to make yearOfBirth = to new function and then I only need 
 //the one input of age in the console.log statement.
-function getYearOfBirth(age){
-  return 2019 - age;
-}
-//variable is invoking function, capturing returned value, and logging it out.
-// const greeting1 = createGreeting();
 
-// console.log(greeting1);
-console.log(createGreeting('JT',37));
+//variable is invoking function, capturing returned value, and logging it out.
+try {
+  const greeting1 = createGreeting('JT', 37);
+  console.log(greeting1);
+} catch(e) {
+  console.error(e.message);
+}
