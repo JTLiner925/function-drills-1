eb38111722916e70f2387084d5720ad91d55e9a2
JTLiner925 <[jtliner925@gmail.com]>
2020-01-07 11:00:03 -0600

    added beyond function

--- function-drills-2.js
+++ function-drills-2.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 
 
 function jediName(firstName, lastName){
@@ -8,5 +9,19 @@ function jediName(firstName, lastName){
   return `${lastName.slice(0,3)}${firstName.slice(0,2)}`;
 }
 
-  const jedi = jediName('John', 'Liner');
-  console.log(jedi);
+const jedi = jediName('John', 'Liner');
+console.log(jedi);
+
+function beyond(num){
+  if(num === Number.NEGATIVE_INFINITY || num === Number.POSITIVE_INFINITY){
+    console.log('And beyond');
+  } else if (Number.isFinite(num) && num > 0){
+    console.log('To infinity');
+  } else if (Number.isFinite(num) && num < 0){
+    console.log('To negative infinity');
+  } else {
+    console.log('Staying home');
+  }
+}
+
+beyond(0);
