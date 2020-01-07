07b4f5e1310e1b8116194a49dc2472feb9e36e89
JTLiner925 <[jtliner925@gmail.com]>
2020-01-07 14:29:16 -0600

    added filter function

--- high-order-functions.js
+++ high-order-functions.js
@@ -13,3 +13,35 @@ function sayGoodbye (){
 }
 
 repeat(sayGoodbye, 5);
+
+
+
+function filter(arr, fn) {
+  const newArray = [];
+  for(let i=0; i<arr.length; i++) {
+    const currentElement = arr[i];
+    if (fn(currentElement) === true ) {
+      newArray.push(currentElement);
+    }
+  }
+  return newArray;
+  // TASK: Define your function here
+}
+
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Roe', 'Bhaumik', 'Ray'];
+
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+
+  //   bonus one-liner:
+  // console.log(filter(names, name => name[0] === 'R'));
+  return name[0] === 'R';
+});
+
+console.log(filteredNames); // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
