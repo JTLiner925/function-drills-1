83d71e5779222c3bcfbddb2486822162604dc758
JTLiner925 <[jtliner925@gmail.com]>
2020-01-07 16:21:02 -0600

    added hazardWarningCreator function

--- high-order-functions.js
+++ high-order-functions.js
@@ -45,3 +45,22 @@ const filteredNames = filter(myNames, function(name) {
 
 console.log(filteredNames); // => ['Rich', 'Ray']
 // <---- DO NOT EDIT BETWEEN THESE LINES
+
+function hazardWarningCreator(typeOfWarning){
+  let warningCounter = 0;
+  return location => {
+    warningCounter++;
+    console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
+    console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} time(s) today!`);
+  };
+}
+
+const rocksWarning = hazardWarningCreator('Rocks on the Road');
+const manholeWarning = hazardWarningCreator('Manhole Cover Uncovered');
+const godzillaWarning = hazardWarningCreator('Godzilla on the Loose');
+
+rocksWarning('Main St');
+rocksWarning('Smith Ave');
+godzillaWarning("King's Road");
+
+// console.log(`The ${typeOfWarning} has triggered ${warningCounter} ${warningCounter === 1 ? 'time' : 'times'}.`);
