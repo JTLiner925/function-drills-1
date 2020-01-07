3ccb2e1f246adbeda292200667c21bcf23f67666
JTLiner925 <[jtliner925@gmail.com]>
2020-01-07 11:38:04 -0600

    added decodeWords function

--- function-drills-2.js
+++ function-drills-2.js
@@ -25,3 +25,41 @@ function beyond(num){
 }
 
 beyond(0);
+
+
+function decode(word) {
+  switch (word[0]){
+  case 'a':
+    return word[1];
+  case 'b':
+    return word[2];
+  case 'c':
+    return word[3];
+  case 'd':
+    return word[4];
+  default:
+    return ' ';     
+  }
+}
+console.log(decode('cable'));
+
+function decodeWords(words) {
+  return words.split(' ').map(word => decode(word)).join('');
+}
+
+console.log(decodeWords('craft block argon meter bells brown croon droop'));
+
+/*
+this string in decodeWords(words) is turned into an array
+of strings when we use the split() method 
+['craft', 'block', 'argon', 'meter', 'bells', 'brown', 'croon', 'droop']
+
+now we map through the array, essentially it is a for loop that applies a condition 
+to each word or item it iterates through. In this case we apply the decode(word) function to each
+word in the new array of strings, which then gives us what you see below.
+ f       o      r      _      l      o      o      p
+
+ last we use the join() method to create a new string by concatenating all 
+ of the elements in an array. finished product looks like this:
+ for loop
+ */
